f5682d2247fb6e8d677e6c4c20245c45642bda52
zihad2022 <zihadulislamafnan@gmail.com>
2025-04-09 23:20:40 +0600

    add delete button

--- app/Filament/Resources/ContactResource.php
+++ app/Filament/Resources/ContactResource.php
@@ -226,6 +226,7 @@ class ContactResource extends Resource
             ->actions([
                 Tables\Actions\EditAction::make(),
                 Tables\Actions\ViewAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
