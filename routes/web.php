2c67001397b0b89ab6e21a0f44fe96d4c3413f61
zihad2022 <zihadulislamafnan@gmail.com>
2025-04-09 23:07:32 +0600

    Inidtial commit

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
+Route::get('/', function () {
+    return redirect(route('contact.create'));
+});
 
 Route::get('contact', [ContactController::class, 'create'])->name('contact.create');
 Route::post('contact', [ContactController::class, 'store'])->name('contact.store');
